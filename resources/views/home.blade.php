96e86675eedecfd386132ed620db77364be19d4a
danielevergani <dvergani94@gmail.com>
2021-05-07 18:02:49 +0200

    termiato footer, da inserire dati corretti

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -87,6 +87,45 @@
     </main>
 
     <footer>
+        <div class="container">
+            <div class="column">
+                <div class="footer__logo">
+                    <img src="{{asset('img/marchio-sito-test.png')}}" alt="logo">
+                </div>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, quibusdam. Rerum, voluptates doloribus minima odio aspernatur ullam incidunt inventore ut beatae minus fuga, cupiditate exercitationem possimus dicta dolor quos quisquam?</p>
+            </div>
+            <div class="column">
+                <h3>PASTIFICIO</h3>
+                <ul>
+                    <li><a href="#">Lorem ipsum dolor sit a</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit sum dolor sia</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit a</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit sum dolor sisum dolor sia</a></li>
+                    <li><a href="#">Lorem ipsum dolor sitsum dolor si a</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit a</a></li>
+                    <li><a href="#">Lorem ipsum dolorsum dolor si sit a</a></li>
+                </ul>
+                <h3>PRODOTTI</h3>
+                <ul>
+                    <li><a href="#">Lorem ipsum dolor sit a</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit sum dolor sia</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit a</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit sum dolor sisum dolor sia</a></li>
+                    <li><a href="#">Lorem ipsum dolor sitsum dolor si a</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit a</a></li>
+                    <li><a href="#">Lorem ipsum dolorsum dolor si sit a</a></li>
+                </ul>
+            </div>
+            <div class="column">
+                <h3>PRODOTTI</h3>
+                <ul>
+                    <li><a href="#">Lorem ipsum dolor sit a</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit sum dolor sia</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit a</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit sum dolor sisum dolor sia</a></li>
+                </ul>
+            </div>
+        </div>
 
     </footer>
     
