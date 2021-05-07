53703b21339f3a86178599c02cf63a600cab7aa4
danielevergani <dvergani94@gmail.com>
2021-05-07 16:10:32 +0200

    header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <title>Document</title>
+    <title>Molisana</title>
 </head>
 <body>
     <header>
-
+        <div class="logo">
+            <img src="{{asset('img/marchio-sito-test.png')}}" alt="logo">
+        </div>
+        <nav>
+            <ul>
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Prodotti</a></li>
+                <li><a href="#">News</a></li>
+            </ul>
+        </nav>
     </header>
 
     <main>
@@ -17,7 +26,7 @@
     </main>
 
     <footer>
-        
+
     </footer>
     
 </body>
