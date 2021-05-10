3d543a32444aba507f0ce16613a55ddda3b3dda8
danielevergani <dvergani94@gmail.com>
2021-05-10 18:13:37 +0200

    nav con active dinamica

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -1,12 +1,13 @@
 <header>
+   
     <div class="logo">
         <img src="{{asset('img/marchio-sito-test.png')}}" alt="logo">
     </div>
     <nav>
         <ul>
-            <li class="active"><a href="{{route('home')}}">Home</a></li>
-            <li><a href="#">Prodotti</a></li>
-            <li><a href="{{route('news')}}">News</a></li>
+            <li class="{{ Route::getCurrentRoute()->getName() == 'home' ? 'active' : '' }}"><a href="{{route('home')}}">Home</a></li>
+            <li class="{{ Route::getCurrentRoute()->getName() == 'prodotti' ? 'active' : '' }}"><a href="{{route("prodotti", ['id' => 0])}} ">Prodotti</a></li>
+            <li class="{{ Route::getCurrentRoute()->getName() == 'news' ? 'active' : '' }}"><a href="{{route('news')}}">News</a></li>
         </ul>
     </nav>
 </header>
