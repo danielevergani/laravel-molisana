540adaf389af6190c95adb5693de2d3cb7ae3340
danielevergani <dvergani94@gmail.com>
2021-05-10 18:00:08 +0200

    inserite arrow per slider

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -1,3 +1,7 @@
+@section('fontAwesomeCdn')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+@endsection
+
 @extends('template/main')
 
 @section('css')
@@ -14,6 +18,14 @@
     <div class="prodotto">
         <h1 class="prod__name"> {{$pasta['titolo']}} </h1>
         <img src="{{$pasta['src-h']}}" alt="">
+        <div class="arrows">
+            <div class="arrow prev">
+                <i class="fas fa-chevron-left"></i>
+            </div>
+            <div class="arrow next">
+                <i class="fas fa-chevron-right"></i>
+            </div>
+        </div>
         <img src="{{$pasta['src-p']}}" alt="{{$pasta['titolo']}}">
         <div class="description">
             <p>{!!$pasta['descrizione']!!}</p>
--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        @yield('fontAwesomeCdn')
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <title>@yield('title')</title>
         @yield('css');
